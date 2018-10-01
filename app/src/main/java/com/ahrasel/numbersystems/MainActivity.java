420a58f532fd9ae144c26196d740dc5ae1a6775b
ahrasel <rasel.lumia@live.com>
2018-10-01 14:22:10 +0600

    Change Menu Item Name

--- app/src/main/java/com/ahrasel/numbersystems/MainActivity.java
+++ app/src/main/java/com/ahrasel/numbersystems/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_convert_number) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_Number_calculation) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_all_ascii_code) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_bcd_code) {
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_rateus) {
 
         }
 
